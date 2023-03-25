9d471d3355edc0bb2e762e91951dfe5f78ef7304
Irina-Belyavskaya <ir200210b@mail.ru>
2023-03-25 18:21:20 +0300

    refactoring

--- src/app/auth/auth-sign-in.page.tsx
+++ src/app/auth/auth-sign-in.page.tsx
@@ -3,11 +3,18 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Form from "./auth-form.component";
 import { signInUser } from "./store/auth.actions";
+import { useAuthSelector } from "./store/auth.selectors";
 import { SignInDto } from "./types/sign-in-dto.type";
 
 export default function AuthSignInPage() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const {errors} = useAuthSelector();
+
+  useEffect(() => {
+    if (errors.token)
+      alert(errors.token);
+  }, [errors])
   
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -22,10 +29,8 @@ export default function AuthSignInPage() {
     };
 
     dispatch(signInUser({dto}))
-      .then((response: any) => {
-        if (response.meta.rejectedWithValue) {
-          console.log(response.payload);
-        } else {
+      .then(({meta}) => {
+        if (meta.requestStatus !== 'rejected') {
           currentTarget.reset();
           navigate('/', {replace: true});
         }
--- src/app/auth/auth-sign-up.page.tsx
+++ src/app/auth/auth-sign-up.page.tsx
@@ -1,17 +1,25 @@
 import { useAppDispatch } from "hooks/redux";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Form from "./auth-form.component";
 import { registerUser } from "./store/auth.actions";
+import { useAuthSelector } from "./store/auth.selectors";
 import { RegistrationDto } from "./types/registration-dto.type";
 
 export default function AuthSignUpPage() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const {errors} = useAuthSelector();
+
+  useEffect(() => {
+    if (errors.token)
+      alert(errors.token);
+  }, [errors])
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const currentTarget = event.currentTarget;
-    
+
     const data = new FormData(event.currentTarget)
     const email : string = String(data.get('email'));
     const password : string = String(data.get('password'));
@@ -23,10 +31,8 @@ export default function AuthSignUpPage() {
     };
 
     dispatch(registerUser({dto}))
-      .then((response: any) => {
-        if (response.meta.rejectedWithValue) {
-          console.log(response.payload);
-        } else {
+      .then(({meta}) => {
+        if (meta.requestStatus !== 'rejected') {
           currentTarget.reset();
           navigate('/', {replace: true});
         }
