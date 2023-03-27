78bae855fd6c7721959d3cb27fe8acfbd6bc60d6
Алексей <borichevskiy03@mail.ru>
2023-03-27 20:11:51 +0300

    added refresh-token

--- src/repository.ts
+++ src/repository.ts
@@ -7,6 +7,15 @@ const repository = axios.create({
     baseURL: REACT_APP_API_URL
 });
 
+
+const headers = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+}
+
+
 repository.interceptors.request.use(
     (config) => {
       const access_token = Cookies.get('access_token');
@@ -16,5 +25,26 @@ repository.interceptors.request.use(
       return config;
     },
   );
+
+repository.interceptors.response.use((config) => {
+    return config;
+}, async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await repository.get("auth/refresh-token", headers);
+            Cookies.set('access_token', response.data.access_token);
+            return repository.request(originalRequest);
+        }
+        catch (e) {
+            console.log("Не авторизован");
+        }
+    }
+    throw error;
+    }
+)
+
+
 
 export default repository;
